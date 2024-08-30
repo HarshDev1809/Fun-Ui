b6eddb566f6fcb7b2989a2e56249b1b124350918
HarshDev1809 <harsh.dev182000@gmail.com>
2024-08-31 05:15:39 +0530

    new commit: photos added.

--- src/components/Emoticons/ClassicEmoticon/ClassicEmoticon.js
+++ src/components/Emoticons/ClassicEmoticon/ClassicEmoticon.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 import { classicEmoticons } from "../../../utils/constants/emoticons";
 import {invalidVariant} from "../../../utils/helperFunctions/errors";
 import { emoticonDefaultStyle } from "../../../utils/constants/defaultStyle";
+import { createUseStyles } from "react-jss";
 
-function ClassicEmoticon({ variant, style }) {
+function ClassicEmoticon({ variant, style, rotate}) {
   if (!variant) {
     variant = 1;
   }
+
+  const useStyle = createUseStyles({
+    rotated : {
+        transform: "rotate(90deg)",
+    },
+})
+
+
   invalidVariant(variant,Object.keys(classicEmoticons))
   const [emoticon, setEmoticon] = useState(classicEmoticons[variant]);
   const finalStyle = { ...emoticonDefaultStyle, ...style };
-
+  const classes = useStyle()
   return (
-    <span className="classic-emoticon" style={finalStyle}>
+    <span className={`classic-emoticon ${rotate? "" : classes.rotated}`} style={finalStyle}>
       {emoticon}
     </span>
   );
